18975e9e3f9548b0138dc508216a7d3906cd71e1
AlejandroBlanco2001 <nuclearhands2001@gmail.com>
2020-03-12 20:53:25 -0500

    Version 1.0
    Agregamos el GameLoop y la ventana del juego

---
+++ src/pruebas/CanvasG.java
@@ -0,0 +1,18 @@
+package pruebas;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+public class CanvasG extends Canvas{
+    
+    public CanvasG(){
+        setPreferredSize(new Dimension(1080,720));
+    }
+    
+    public void paint(Graphics g){
+
+    }
+    
+}
